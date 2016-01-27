71af053b0eac9118616ec262189e92f423cebcdb
o.akbas <ozlemakbas90@gmail.com>
2016-01-27 16:54:25 +0200

    Client Code organized

--- src/IRCClient.java
+++ src/IRCClient.java
@@ -1,13 +1,31 @@
 import java.io.*;  
 import java.net.*;
 
-
 public class IRCClient {
+	private Socket clientSoc;
+	private String hostname;
+	private int port;
 	
-	public static void main(String[] args) {
+	public IRCClient (int port, String hostname) {
+		this.port = port;
+		this.hostname = hostname;
+	}
+	
+	public void connectToServer() {
+		
 		Socket clientSoc = null;
+		
+	    try {
+			clientSoc = new Socket(hostname, port);
+		} catch (UnknownHostException e1) {
+			// TODO Auto-generated catch block
+			System.out.println("Unknown host");
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			System.out.println("Can not set connection");
+		}
+		
 		try {
-		    clientSoc = new Socket("localhost", 12345);
 		    PrintWriter out = new PrintWriter(clientSoc.getOutputStream(), true);
 		    BufferedReader in = new BufferedReader(new InputStreamReader(clientSoc.getInputStream()));
 		    System.out.println(in.toString());
@@ -16,5 +34,12 @@ public class IRCClient {
 		    System.out.println("Read failed");
 		    System.exit(1);
 		}
+	}
+	
+	public static void main(String[] args) {
+		int port = 12345;
+		String hostname = "localhost";
+		IRCClient clientSoc = new IRCClient(port,hostname);
+		clientSoc.connectToServer();
 	}
 }
